184dbdb15c737e603e2d7099ce6ca5c76aa01483
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-12-25 14:28:46 +0700

    connect the test_flaskr.py to the 'trivia_test' database by using credentials in .env file

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,7 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -15,7 +17,9 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_username = os.getenv('DB_USERNAME')
+        self.database_password = os.getenv('DB_PASSWORD')
+        self.database_path = "postgresql+psycopg2://{}:{}@{}/{}".format(self.database_username, self.database_password, 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
